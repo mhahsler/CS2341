693e95f942cde0606b73adc862bdc149e169e17d
Michael Hahsler <michael@hahsler.net>
2022-06-21 14:06:36 -0500

    add link

--- Chapter1_Overview/big-five/main.cpp
+++ Chapter1_Overview/big-five/main.cpp
@@ -14,6 +14,8 @@ using namespace std;
  * Typically the default provided by C++ works.
  * Exception: If a member variable contains a pointer (i.e., we use dynamic memory allocation with new/delete)
  *
+ * General info on operator overloading: https://en.cppreference.com/w/cpp/language/operators
+ *
  * Run with debugger: Set breakpoint to the first line of main() and watch the variabels.
  */
 
