208212d033fb71f0fda89b445d8099424d18b1aa
Michael Hahsler <michael@hahsler.net>
2022-06-21 12:10:28 -0500

    added description.

--- Chapter1_Overview/class_templates/main.cpp
+++ Chapter1_Overview/class_templates/main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 /**
  * A class for simulating a memory cell.
+ * 
+ * It is assumed that Object has:
+ *  1. a zero-parameter constructor (used in line 18)
+ *  2. a copy constructor (if read is used in object contruction)
+ *  3. a copy assignment operator (used in line 26)
+ * 
+ * for many complilers, the entire template class with the implementation has to be put into a single .h file.
  */
 template <typename Object>
 class MemoryCell
