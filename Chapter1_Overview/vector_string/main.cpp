aeb1523ac39ebe0b8595fc4386cde000b25093aa
Michael Hahsler <michael@hahsler.net>
2022-06-23 09:32:06 -0500

    added cstring examples.

--- Chapter1_Overview/vector_string/main.cpp
+++ Chapter1_Overview/vector_string/main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <string.h>
 
 using namespace std;
 
-
 /**
- * @brief Use STD library vectors and string! 
- * 
- * @return int 
+ * @brief Use STD library vectors and string!
+ *
+ * @return int
  */
 int main()
 {
+    // Array vs STL vector
 
-    /* C+11 lets us initialize vectors like this */
+    /* C-style arrays are just pointers to memory (first element of the array).
+     * C-style arrays have several drawbacks:
+     * 1. Arrays cannot be assigned with = (they are pointers)
+     * 2. Arrays do not know its length.
+     * 3. Arrays cannot be dynamically enlarged.
+     */
+
+    int arr1[] = {0, 1, 2, 3, 4};
+
+    for (int i = 0; i < 5; ++i)
+        cout << arr1[i];
+    cout << endl;
+
+    // needs dynamic allocation if the length needs to be decided at run time.
+    // In C you will see calls to malloc() and free() instead of new and delete.
+    int n = 20;
+    int *arr2 = new int[n];
+
+    memcpy(arr2, arr1, 5 * sizeof(*arr1));
+
+    for (int i = 0; i < n; ++i)
+        cout << arr2[i];
+    cout << endl;
+
+    delete[] arr2;
+    arr2 = NULL; // make sure we do not use the pointer anymore!
+
+    /* STL vectors fix the issues and should be used instead
+     * C++11 lets us initialize vectors like this
+     */
     vector<int> squares = {1, 4, 9, 16, 25};
 
     /* Vectors know how long they are and check boundaries */
@@ -37,13 +67,41 @@ int main()
 
     cout << "The sum is " << sum << endl;
 
-    /* strings */
+    // cstring vs. STL string
+
+    /* cstrings are character arrays terminated with `\0`.
+     * #include <string.h>
+     * provides many useful functions (see: https://cplusplus.com/reference/cstring/)
+     */
+    char cstr1[10];
+
+    char cstr2[] = "Hello!";
+
+    strcpy(cstr1, cstr2);
+
+    if (cstr1 == cstr2)
+        cout << cstr1 << " == " << cstr2 << endl;
+    else
+        cout << cstr1 << " != " << cstr2 << endl;
+
+    /* STL strings */
     string a = "Michael", b = "Michae";
 
     if (a == b)
         cout << a << " == " << b << endl;
+    else
+        cout << a << " != " << b << endl;
 
     b += "l";
     if (a == b)
         cout << a << " == " << b << endl;
+    else
+        cout << a << " != " << b << endl;
+
+    /* get a cstring copy from a STL string */
+    char *cstr = new char[a.length() + 1];
+    strcpy(cstr, a.c_str());
+    cout << "As cstring: " << cstr << endl;
+
+    return 0;
 }
