8773972fa0688b64582e6d10c23f25a9ce9cfb42
Michael Hahsler <michael@hahsler.net>
2022-09-13 16:51:37 -0500

    added string example.

---
+++ Chapter3_Lists_etc/DSString/main.cpp
@@ -0,0 +1,56 @@
+#define DEBUG 1
+
+#include <iostream>
+#include <algorithm>
+
+#include "DSString.h"
+#include "DSVector.h"
+
+using namespace std;
+
+int main()
+{  
+    DSString myString = "Hello, World!";
+    cout << myString << endl;
+
+    // this uses the one argument constructor in DSString and then the inherited operator=
+    myString = "Good bye!";
+    cout << myString << endl;
+
+    cout << "substr: " << myString.substring(5, 3) << endl;
+
+    DSString a = "test";
+    DSString b = a;
+
+    cout << boolalpha;
+    cout << (a == b) << endl;
+
+    // use initialization list (see DSVector.h)
+    DSVector<DSString> strings = {
+        DSString("bbb"),
+        DSString("aaa"),
+        DSString("ddd"),
+        DSString("eee"),
+        DSString("ccc")};
+
+    // find strings
+    for (const auto &s : strings)
+        cout
+            << s << "\n";
+
+    cout << "found ddd: " << (find(strings.begin(), strings.end(), DSString("ddd")) != strings.end()) << endl;
+    cout << "found zzz: " << (find(strings.begin(), strings.end(), DSString("zzz")) != strings.end()) << endl;
+
+    // sorting
+    sort(strings.begin(), strings.end());
+
+    for (const auto &s : strings)
+        cout
+            << s << "\n";
+
+    // now we can do more efficient search
+    cout << "found ddd: " << binary_search(strings.begin(), strings.end(), DSString("ddd")) << endl;
+    cout << "found zzz: " << binary_search(strings.begin(), strings.end(), DSString("zzz")) << endl;
+
+    return 0;
+}
