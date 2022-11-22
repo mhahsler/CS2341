648150869af9c5a5f97aa938a3dcdf791127ff3e
Michael Hahsler <michael@hahsler.net>
2022-11-22 10:37:34 -0600

    improved representations.

--- Chapter9_Graphs/graph/main.cpp
+++ Chapter9_Graphs/graph/main.cpp
@@ -5,54 +5,84 @@
 
 // You need boost installed
 #include <boost/functional/hash/hash.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
 
 using namespace std;
 
+
+// Simple example where vertices are represented as integers (i.e., vertex ID).
+
 int main()
 {
+    // define types for more readable code
     typedef int vertex;
     typedef pair<vertex, vertex> edge;
 
-    // tuple representation
+    // Representation 1: Tuple representation (a vector of edge tuples)
     vector<vertex> vertices = {0, 1, 2, 3};
     vector<edge> edges = {edge(0, 1), edge(0, 2), edge(1, 2), edge(2, 3)};
+    
+    cout << "Tuple representation (vector):\n";
+    for (const auto &e : edges)
+        cout << "(" << e.first << ", " << e.second << ")" << "\n";
+    cout << endl;
 
-    // Adjacency matrix is just a simple matrix, but takes too much memory
+    //Note: searching the vector for an edge is O(|E|).
+    
+    // Representation 2: Tuple representation (a hash of edge tuples)
+    
+    // Notes: * unordered_set is a hash so we get O(1) access.
+    //        * the STL does not provide hash functions for pairs. Boost libraries provide boost::hash.
+    //        * Constructor for a load factor of 1 (table size should be N)
+    unordered_set<edge, boost::hash<edge>> edges_hashed = unordered_set<edge, boost::hash<edge>>(edges.size());
+    for (auto &e : edges)
+        edges_hashed.insert(e);
 
-    // Adjacency List
+    cout << "Tuple representation (hash):\n";
+    for (auto &e : edges_hashed)
+        cout << "(" << e.first << ", " << e.second << ")\n";
+
+    // find edges fast
+    cout << std::boolalpha;
+    cout << "1 -> 2? " << (edges_hashed.find(edge(1, 2)) != edges_hashed.end()) << "\n";
+    cout << "1 -> 3? " << (edges_hashed.find(edge(1, 3)) != edges_hashed.end()) << "\n";
+    cout << endl;
+
+    
+    // Representation 3: Adjacency List (vector of singly-linked lists)
     vector<forward_list<vertex>> adjacencyList = vector<forward_list<vertex>>(vertices.size());
     for (auto &e : edges)
         adjacencyList[e.first].push_front(e.second);
 
-    // Notes: forward_list is singly linked
 
-    cout << "adjacency list:\n";
+    cout << "Adjacency list:\n";
     for (size_t v = 0; v < adjacencyList.size(); ++v)
     {
         cout << v << ":";
-        for (auto &v2 : adjacencyList[v])
+        for (const auto &v2 : adjacencyList[v])
             cout << " " << v2;
         cout << "\n";
     }
-    cout << "\n";
+    
+    cout << endl;
 
-    // Dictionary of keys
-    unordered_set<edge, boost::hash<edge>> dictOfKeys = unordered_set<edge, boost::hash<edge>>(edges.size());
+
+
+    // Note: fast lookup by vertex ID (O(1))!
+
+
+    // Representation 4: Adjacency matrix
+    boost::numeric::ublas::matrix<int> adjacencyMatrix(vertices.size(), vertices.size());
     for (auto &e : edges)
-        dictOfKeys.insert(e);
+        adjacencyMatrix(e.first, e.second) = 1;    
+    
+    cout << "Adjacency matrix:\n";
+    cout << adjacencyMatrix << endl;
 
-    // Notes: * unordered_set is a hash so we get O(1) access.
-    //        * the STL does not provide hash functions for pairs. Boost libraries do.
-    //        * Constructor for a load factor of 1 (table size should be N)
-
-    // print all edges. loop can also be used to dump the has into a tuple presentation.
-    cout << "edges in dictionary:\n";
-    for (auto &e : dictOfKeys)
-        cout << e.first << " -> " << e.second << "\n";
-
-    // try an non-existing edge
-    cout << std::boolalpha;
-    cout << "4 -> 1? " << (dictOfKeys.find(edge(4, 1)) != dictOfKeys.end()) << "\n\n";
+    // Note: dense adjacency representation are easy to work with using row/col indices, but they
+    // only works for small graphs with few vertices since it is O(|V|^2) in space. 
+    // Use one of the other representations or a sparse matrix representation.
 
     return 0;
 }
