d86c80087d1b9e9e3b78eddb2ad1ced1814e305d
Michael Hahsler <michael@hahsler.net>
2023-01-30 15:57:20 -0600

    added array.

--- Chapter1_Programming/pointers/main.cpp
+++ Chapter1_Programming/pointers/main.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main()
 {
     // We can statically allocate a IntCell object on the stack.
+    cout << "\nStatic Memory Allocation:\n";
     IntCell m_stack(3);
     cout << "Cell m_stack: " << m_stack.read() << "\n";
     
     // We dynamically allocate a IntCell object
+    cout << "\nDynamic Memory Allocation:\n";
     // Note that this is not necessary, since we know at compile time how much memory we have!
     // To allocate the memory on the stack, we would use:
     // IntCell m{0}; 
@@ -24,7 +26,7 @@ int main()
     IntCell *n = new IntCell{5};
     cout << "Cell n: " << n->read() << "\n";
 
-    /* comparison does not work as expected, it compares the pointers */
+    /* comparison does not work as expected, it compares the pointers/addresses */
     if (m == n)
         cout << "m == n" << "\n";
     else
@@ -33,10 +35,11 @@ int main()
 
     /* addresses and dereferencing */
     cout << "m: " << m << "\n";
-    cout << "Address of m (&m): " << &m << "\n";
-    cout << "Dereferenced m ( *m.read() ): " << (*m).read() << "\n";
+    cout << "n: " << n << "\n";
+    cout << "Value of m ( m->read() ): " << m->read() << "\n";
+    cout << "Value of m using the dereferencing operator ( (*m).read() ): " << (*m).read() << "\n";
 
-    /* this works */
+    /* this works for comparing*/
     if (m->read() == n->read())
         cout << "m->read() == n->read()" << "\n";
     else
@@ -45,7 +48,10 @@ int main()
     /* every new needs a delete! */
     delete m, n;
 
-    //  References (lvalue references)
+   
+    // References (lvalue references)
+    cout << "\nReferences:\n";
+
     // & used in a declaration (not as an operator like above!!!)
     // used as an alias (avoid copying memory, e.g., for a function call)
     // `auto` can be used. The compiler figures out the data type.
@@ -53,10 +59,29 @@ int main()
     auto &b = a;
 
     if (&a == &b)
-        cout << "a and b occupy the same memory!" << "\n";
+        cout << "a and b occupy the same memory: " << &a << "\n";
 
     b = 24;
     cout << "a: " << a << "\n";
+
+    // Pointer Arithmetic: Arrays and Pointers
+    cout << "\nPointers and Arrays:\n";
+    IntCell arr[] = {IntCell{100}, IntCell{101}, IntCell{102}, IntCell{103}, IntCell{104}};
+    // we could also dynamically allocate the array on the heap:
+    // IntCell *arr = new IntCell[5];
+
+
+    cout << "arr[2].read(): " << arr[2].read() << "\n";
+    cout << "(arr + 2)->read(): " << (arr + 2)->read() << "\n";
+    cout << "(*(arr + 2)).read(): " << (*(arr + 2)).read() << "\n";
+
+    IntCell* arr_ptr = arr; // array name can be used to get a pointer to the first element
+
+    arr_ptr += 2; // move two positions forward
+    cout << "arr_ptr (after +2): " << arr_ptr->read() << "\n";
+
+    --arr_ptr; // move one positions back
+    cout << "arr_ptr (after -1): " << arr_ptr->read() << "\n";
 
     return 0;
 }
