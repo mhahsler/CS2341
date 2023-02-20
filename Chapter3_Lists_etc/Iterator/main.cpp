7731182889ee13b683f38ebb35219770173ac495
Michael Hahsler <michael@hahsler.net>
2023-02-20 17:46:22 -0600

    cleaned code.

--- Chapter3_Lists_etc/Iterator/main.cpp
+++ Chapter3_Lists_etc/Iterator/main.cpp
@@ -12,47 +12,46 @@ int main()
 
     // Using the index
     int sum = 0;
-    for (int i = 0; i < squares.size(); ++i)
+    for (size_t i = 0; i < squares.size(); ++i)
         sum += squares[i];
 
     // at() is [] with boundary check
-    // cout << squares.at(100) << endl;
+    // cout << squares.at(100) << "\n";
 
-    cout << "The sum (index) is " << sum << endl;
+    cout << "The sum (vector + index) is " << sum << "\n";
 
      // Using iterators
     sum = 0;
     for (vector<int>::iterator pos = squares.begin(); pos != squares.end(); ++pos)
-        sum += *pos;
+        sum += *pos;   // iterators use * like pointers
 
-    cout << "The sum (iterator) is " << sum << endl;
-    // Note that pos < squares.end() does not work!
+    cout << "The sum (vector + iterator) is " << sum << "\n";
+    // Note that pos < squares.end() does not work for iterators!
 
 
-    // Use C++11 ranges. x is a reference to the element in squares.
+    // Use C++11 range. x is a reference to the element in squares.
+    // C++ deduces data type for auto.
     sum = 0;
     for (const auto &x : squares)
         sum += x;
 
-    cout << "The sum (ranges) is " << sum << endl;
+    cout << "The sum (vector + range) is " << sum << "\n";
 
-    // list instead of vector
+    // Use a list instead of vector. 
+    // Index does not work for list, but iterator and range do with the same code.
     list<int> squaresList = {1, 4, 9, 16, 25};
     
     sum = 0;
     for (auto pos = squaresList.begin(); pos != squaresList.end(); ++pos)
         sum += *pos;
 
-    cout << "The sum (list + iterator) is " << sum << endl;
-    // C++ deduces data type for auto.
+    cout << "The sum (list + iterator) is " << sum << "\n";
 
-
-    // C++11 ranges also work, but index does not!
-     sum = 0;
+    sum = 0;
     for (const auto &x : squaresList)
         sum += x;
 
-    cout << "The sum (list + ranges) is " << sum << endl;
+    cout << "The sum (list + ranges) is " << sum << "\n";
 
    return 0;
 }
