3cbc7ad10a45b99a9a18478a4ed341a8ad6a0387
Michael Hahsler <michael@hahsler.net>
2023-02-22 12:08:25 -0600

    removed using namespace.

--- Chapter2_Algorithm_Analysis/STL_sort/timing.cpp
+++ Chapter2_Algorithm_Analysis/STL_sort/timing.cpp
@@ -5,24 +5,22 @@
 #include <chrono>
 #include <ctime>
 
-using namespace std;
-using namespace std::chrono;
 
 // Function to print a vector
 template <typename T>
-void printVector(const vector<T> &v)
+void printVector(const std::vector<T> &v)
 {
     for (auto &e : v)
-        cout << e << " ";
-    cout << endl;
+        std::cout << e << " ";
+    std::cout << std::endl;
 }
 
 // This return is efficient in C++ because it uses move (see std::move) 
 //instead of copy.
-vector<int> randomIntVector(size_t size)
+std::vector<int> randomIntVector(size_t size)
 {
-    vector<int> v(size);
-    generate(v.begin(), v.end(), std::rand);
+    std::vector<int> v(size);
+    std::generate(v.begin(), v.end(), std::rand);
     return (v);
 }
 
@@ -31,26 +29,26 @@ int main()
 {
     srand(time(NULL));
 
-    cout << "N, time [micro sec.]" << endl;
+    std::cout << "N, time [micro sec.]" << "\n";
 
     
     // Note that this goes up to 10 million!
-    for (size_t N = 1; N <= 10000000; N *= 10)
-    // for (int N = 1; N <= 20000; N += 1000)
+    //for (size_t N = 1; N <= 10000000; N *= 10)
+    for (int N = 1; N <= 20000; N += 1000)
     {
-        vector<int> v = randomIntVector(N);
+        std::vector<int> v = randomIntVector(N);
 
-        auto start = high_resolution_clock::now();
+        auto start = std::chrono::high_resolution_clock::now();
 
         // sorting a container (see https://cplusplus.com/reference/algorithm/sort/)
         sort(v.begin(), v.end());
         // printVector(v);
 
-        auto stop = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(stop - start);
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-        cout << N << ", "
-             << duration.count() << endl;
+        std::cout << N << ", "
+             << duration.count() << "\n";
     }
 
     return 0;
