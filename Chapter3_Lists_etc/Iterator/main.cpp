856dcb8059cb4c69fe53eb612fc77ee2ee91c058
Michael Hahsler <michael@hahsler.net>
2023-02-24 09:46:26 -0600

    removed using namespace.

--- Chapter3_Lists_etc/Iterator/main.cpp
+++ Chapter3_Lists_etc/Iterator/main.cpp
@@ -3,12 +3,9 @@
 #include <list>
 
 
-using namespace std;
-
-
 int main()
 {
-    vector<int> squares = {1, 4, 9, 16, 25};
+    std::vector<int> squares = {1, 4, 9, 16, 25};
 
     // Using the index
     int sum = 0;
@@ -18,14 +15,14 @@ int main()
     // at() is [] with boundary check
     // cout << squares.at(100) << "\n";
 
-    cout << "The sum (vector + index) is " << sum << "\n";
+    std::cout << "The sum (vector + index) is " << sum << "\n";
 
      // Using iterators
     sum = 0;
-    for (vector<int>::iterator pos = squares.begin(); pos != squares.end(); ++pos)
+    for (std::vector<int>::iterator pos = squares.begin(); pos != squares.end(); ++pos)
         sum += *pos;   // iterators use * like pointers
 
-    cout << "The sum (vector + iterator) is " << sum << "\n";
+    std::cout << "The sum (vector + iterator) is " << sum << "\n";
     // Note that pos < squares.end() does not work for iterators!
 
 
@@ -35,23 +32,24 @@ int main()
     for (const auto &x : squares)
         sum += x;
 
-    cout << "The sum (vector + range) is " << sum << "\n";
+    std::cout << "The sum (vector + range) is " << sum << "\n";
 
     // Use a list instead of vector. 
     // Index does not work for list, but iterator and range do with the same code.
-    list<int> squaresList = {1, 4, 9, 16, 25};
+    // This is great, because you can change the container without changing the code!
+    std::list<int> squaresList = {1, 4, 9, 16, 25};
     
     sum = 0;
     for (auto pos = squaresList.begin(); pos != squaresList.end(); ++pos)
         sum += *pos;
 
-    cout << "The sum (list + iterator) is " << sum << "\n";
+    std::cout << "The sum (list + iterator) is " << sum << "\n";
 
     sum = 0;
     for (const auto &x : squaresList)
         sum += x;
 
-    cout << "The sum (list + ranges) is " << sum << "\n";
+    std::cout << "The sum (list + ranges) is " << sum << "\n";
 
    return 0;
 }
