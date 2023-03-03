042e4700f7fc3beb13571eee0c6308f858959c59
Michael Hahsler <michael@hahsler.net>
2023-03-03 11:38:24 -0600

    moved to standard fgets.

--- Chapter1_Programming/io/fileIO_cstring_main.cpp
+++ Chapter1_Programming/io/fileIO_cstring_main.cpp
@@ -1,26 +1,19 @@
 // C libraries
 #include <cstdio> // C++ version of C's stdio.h
-#include <cstdlib> // C++ version of C's stdlib.h
+#include <cstring> // for strlen()
 
 // C++ used for printing
 #include <iostream>
 
-// Note: this uses getline() from stdio.h (C) which is different from std::getline() in the C++ <string> library!!!
-// See: https://man7.org/linux/man-pages/man3/getline.3.html
-//
-// Alternatively you can use getc() (also in stdio.h) to read a single character at a time.
-
-       int main()
+// Warning this mixes C file IO with C++ cout! You probably want to 
+// look at how to do both in C++
+int main()
        {
            char filename[] = "./test.txt";
            FILE *stream;
            
            // set up the buffer
-           char buffer[1001]; // add extra space for null terminator, etc.
-           char *b = buffer;  // we need a pointer to the buffer 
-           size_t maxlen = 1000;
-           
-           size_t nread;
+           char buffer[1000]; // maximum line length
 
            stream = fopen(filename, "r");
            if (stream == NULL) {
@@ -28,14 +21,17 @@
                return(-1);
            }
 
-           // getline wants pointers to all its arguments!
-           // size_t getline(char **lineptr, size_t *n, FILE *stream)
-           while ((nread = getline(&b, &maxlen, stream)) != -1) 
-               std::cout << "I read " << nread << " character: " << buffer;
+           /* The C library function char *fgets(char *str, int n, FILE *stream) 
+            * reads a line from the specified stream and stores it into the string 
+            * pointed to by str. It stops when either (n-1) characters are read, the 
+            * newline character is read, or the end-of-file is reached, whichever 
+            * comes first. See https://en.cppreference.com/w/c/io/fgets
+            */
+           while (fgets(buffer, sizeof(buffer), stream) != NULL) 
+               std::cout << "I read " << strlen(buffer) << " character: " << buffer;
             
             std::cout << std::endl;
      
-
            fclose(stream);
            return(0);
        }
