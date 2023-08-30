050d0332724885e21e40c2ea22cc55d94cbffb2b
mhahsler <mhahsler@lyle.smu.edu>
2023-08-30 09:01:22 -0500

    removed using namespace std.

--- Chapter1_Programming/templates/function_templates/main.cpp
+++ Chapter1_Programming/templates/function_templates/main.cpp
@@ -5,8 +5,6 @@
 
 #include "IntCell.h"
 
-using namespace std;
-
 // Template function example
 
 // Figures 1.19 and 1.20, but with illegal lines commented out
@@ -21,7 +19,7 @@ using namespace std;
  * for the data type used in that call.
  */
 template <typename Comparable>
-const Comparable & findMax( const vector<Comparable> & a )
+const Comparable & findMax( const std::vector<Comparable> & a )
 {
     int maxIndex = 0;
 
@@ -34,18 +32,18 @@ const Comparable & findMax( const vector<Comparable> & a )
 
 int main( )
 {
-    vector<int>     v1 = { 3, 5, 8, 2, 4 };
-    vector<double>  v2 = { 2.9, 5.4, 2.1, 3.3 };
-    vector<string>  v3 = { "hello", "world", "apple" };
-    vector<IntCell> v4( 75 );
+    std::vector<int>     v1 = { 3, 5, 8, 2, 4 };
+    std::vector<double>  v2 = { 2.9, 5.4, 2.1, 3.3 };
+    std::vector<std::string>  v3 = { "hello", "world", "apple" };
+    std::vector<IntCell> v4(10);  // create a vector with 10 IntCells, we did not implement list initialization for IntCell.
 
     // the compiler will analyze the calls below, determine the type of the argument, and then
     // generate multiple version of the template function with each needed type. 
-    cout << "Templated findMax() function\n"; 
-    cout << findMax(v1) << endl;  // OK: Comparable = int
-    cout << findMax(v2) << endl;  // OK: Comparable = double
-    cout << findMax(v3) << endl;  // OK: Comparable = string
-//    cout << findMax(v4).read() << endl;  // Illegal; operator< undefined for IntCell
+    std::cout << "Templated findMax() function\n"; 
+    std::cout << findMax(v1) << "\n";  // OK: Comparable = int
+    std::cout << findMax(v2) << "\n";  // OK: Comparable = double
+    std::cout << findMax(v3) << std::endl;  // OK: Comparable = string
+//    std::cout << findMax(v4).read() << std::endl;  // Illegal; operator< undefined for IntCell
 // compiler error!
 // [build] main.cpp: In instantiation of ‘const Comparable& findMax(const std::vector<Comparable>&) [with Comparable = IntCell]’:
 // [build] main.cpp:43:20:   required from here
@@ -57,10 +55,10 @@ int main( )
     // we can call the function as findMax<int>(v1) to force the compiler to use int in the template 
 
     // by the way, the STL already provides https://en.cppreference.com/w/cpp/algorithm/max_element
-    cout << "\nSTL max_element()\n"; 
-    cout << *max_element(v1.begin(), v1.end()) << endl;
-    cout << *max_element(v2.begin(), v2.end()) << endl;
-    cout << *max_element(v3.begin(), v3.end()) << endl;
+    std::cout << "\nSTL max_element()\n"; 
+    std::cout << *max_element(v1.begin(), v1.end()) << "\n";
+    std::cout << *max_element(v2.begin(), v2.end()) << "\n";
+    std::cout << *max_element(v3.begin(), v3.end()) << std::endl;
 
     return 0;
 }
