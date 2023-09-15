5c75e1505d37e8a90ae480522f3bdf70d80f275c
Michael Hahsler <mhahsler@smu.edu>
2023-09-15 08:35:03 -0500

    added try/catch.

--- Chapter1_Programming/factorial/factorial_iterative_main.cpp
+++ Chapter1_Programming/factorial/factorial_iterative_main.cpp
@@ -10,20 +10,29 @@ double factorial_iterative(int number)
 {
     if (number < 0)
         throw std::runtime_error("factorial is not defined for negative numbers!");
-    
+
     double fac = 1;
     for (auto i = number; i > 1; --i)
         fac *= i;
-    
+
     return fac;
 }
 
-int main() {
+int main()
+{
 
-    std::cout << "5! = " << factorial_iterative(5) << "\n";
-    std::cout << "0! = " << factorial_iterative(0) << "\n";  
-    std::cout << "100! = " << factorial_iterative(100) << "\n";
-    std::cout << "(-1)! = " << factorial_iterative(-1) << "\n";
+    // some calls may throw an exception
+    try
+    {
+        std::cout << "0! = " << factorial_iterative(0) << "\n";
+        std::cout << "5! = " << factorial_iterative(5) << "\n";
+        std::cout << "100! = " << factorial_iterative(100) << "\n";
+        std::cout << "(-1)! = " << factorial_iterative(-1) << "\n";
+    }
+    catch (std::runtime_error &e)
+    {
+        std::cerr << "Error: " << e.what() << "\n";
+    }
 
     return 0;
 }
