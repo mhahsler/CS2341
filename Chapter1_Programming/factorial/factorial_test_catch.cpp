3d80206d40fca9ffbcac1c71777bd6b643bc068e
Michael Hahsler <mhahsler@smu.edu>
2024-02-26 12:03:51 -0600

    reordered tests.

--- Chapter1_Programming/factorial/factorial_test_catch.cpp
+++ Chapter1_Programming/factorial/factorial_test_catch.cpp
@@ -19,15 +19,16 @@ TEST_CASE("Simple Factorials", "[factorial]")
     //    (see: https://github.com/mhahsler/CS2341/blob/main/.github/workflows/testing-example_cmake.yml)
     // Remove the preprocessor directives to see the tests fail.
 
-#if false
+#if true
     // Issue cases:
 
-    // 1. The following test fails because we can't test negative numbers because the argument is unsigned! 
+    // 1. The following test fails because we return an unsigned int. Another design issue!
+    CHECK(factorial(100) == 9.332622e+157);
+
+    // 2. The following test fails because we can't test negative numbers because the argument is unsigned! 
     // This is a design issue!
     CHECK_THROWS(factorial(-1));
 
-    // 2. The following test fails because we return an unsigned int. Another design issue!
-    CHECK(factorial(100) == 9.332622e+157);
 #endif
 
 }
